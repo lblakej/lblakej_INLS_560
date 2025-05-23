e52eb9733dc085db10a14d235ad931c1e2f6b214
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-05-23 13:21:55 -0400

    Update 03_expressions.py

--- assignment_01/03_expressions.py
+++ assignment_01/03_expressions.py
@@ -1,19 +1,1 @@
-# Variables
-a = 2
-b = 3
-c = 4
 
-# Default order of operations
-result1 = a + b * c
-print("a + b * c =", result1)
-
-# With parentheses: addition first
-result2 = (a + b) * c
-print("(a + b) * c =", result2)
-
-# More variations
-result3 = a * (b + c)
-print("a * (b + c) =", result3)
-
-result4 = (a + b + c)
-print("(a + b + c) =", result4)
