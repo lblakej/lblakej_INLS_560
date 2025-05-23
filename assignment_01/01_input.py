1912a84ae077c8a62d25cca98e7173e86157815e
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-05-23 13:21:01 -0400

    Update 01_input.py

--- assignment_01/01_input.py
+++ assignment_01/01_input.py
@@ -1,11 +1,2 @@
 # Input exercises 
 
-# first_name = "Larry"
-# last_name = "Jones"
-
-# print(first_name, last_name)
-
-first_name = input("Enter your first name: ") 
-last_name = input("Enter your last name: ")
-
-print(first_name, last_name)
