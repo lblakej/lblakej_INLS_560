cbde46daf22935db108eb8cd4ced66cf603bddc4
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-05-23 13:21:26 -0400

    Update 02_input_conversion.py

--- assignment_01/02_input_conversion.py
+++ assignment_01/02_input_conversion.py
@@ -1,7 +1,1 @@
-quantity_soap = input("How may soaps you want? ")
-hand_soap = 4
-deodorant = 3
-toothpaste = 7
-
-print("I had to buy, hand_soap, deodorant & toothpaste and it cost: $",int(hand_soap) + deodorant + toothpaste)
 
