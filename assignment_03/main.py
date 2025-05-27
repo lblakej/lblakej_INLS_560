ec83000ee5c3577a88b50dc8f8f116a2208e37da
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-05-27 12:23:09 -0400

    Create main.py

---
+++ assignment_03/main.py
@@ -0,0 +1,22 @@
+# Create two or three constants for user input comparison.
+
+CONSTANT_1 = value
+CONSTANT_2 = value
+
+
+# Create two or three variables for provided data
+
+# var1 = int(input()) # if you use an integer 
+# var2 = str(input()) # str is not required if you use text
+
+# write if statement using and (ok to use or if appropriate) example is below:
+
+# if var1 >= CONSTANT_1 and var2 >= CONSTANT_2 :
+     # print("True response")
+# else: 
+    # use an multi-line f-string for the false response. Include your constant variables in the output.
+    # print(f'''False response
+
+You need {CONSTANT_1} Constant one details and {CONSTANT_2}
+details.
+''')
