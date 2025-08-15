3643863ba61143f3f296faae3aa10b0123809eaf
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-08-15 11:11:18 -0400

    Update 08_print_output_various.py

--- assignment_01/08_print_output_various.py
+++ assignment_01/08_print_output_various.py
@@ -1,12 +1,1 @@
-# \n  \t  \'  \" \\
-# concatenation: "string1" + string2"
-# format number: '12,.3f' 1,234.568
 
-string1 = "To be or"
-string2 = "to not to be."
-
-print(string1 + " " + string2)
-
-
-# output
-# To be or to not to be.
