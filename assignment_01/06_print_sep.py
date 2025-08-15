5a379b555ebb12a8b5bffa8b1344168e2ed3c561
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-08-15 11:10:54 -0400

    Update 06_print_sep.py

--- assignment_01/06_print_sep.py
+++ assignment_01/06_print_sep.py
@@ -1,12 +1,1 @@
-# link: http://anh.cs.luc.edu/170/mynotes/sepend.html
 
-print(1, 2, 3)
-print(1, 2, 3, sep=' and ')
-print(4, 5, 6, sep = '')
-print(7, 8, sep='***')
-
-# output
-# 1 2 3
-# 1 and 2 and 3
-# 456
-# 7***8
