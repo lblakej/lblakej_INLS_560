e0e6587a2ad1b00c39bd5748aac6ba56072dae98
Lawrence Jones <32578870+lblakej@users.noreply.github.com>
2025-08-15 11:12:15 -0400

    Update 10_future_value_easy.py

--- assignment_01/10_future_value_easy.py
+++ assignment_01/10_future_value_easy.py
@@ -1,8 +1,1 @@
-# Link: https://lawrencejones-lblakej.cloudapps.unc.edu/inls-560/code/02/02_18_%20future_value
 
-future_value = 600
-rate = 2.0
-years = 10
-present_value = future_value / (1.0 + rate)**years
-
-print('You will need to deposit this amount:', present_value)
